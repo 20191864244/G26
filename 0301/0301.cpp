cdd0af8b9366de25afa2c97771781225dabb7b89
20191864238 <56295781+MinazukiPER@users.noreply.github.com>
2019-10-20 23:13:44 +0800

    38 s homework
    lll

---
+++ 0301/0301.cpp
@@ -0,0 +1,15 @@
+// 0301.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <math.h>
+int main(int argc, char* argv[])
+{
+	float p,r=0.07,n=10;
+	p=pow(1+r,n);
+	printf("p=%f\n",p);
+
+	
+	return 0;
+}
+

c093db863be09d47d5f7ced2fd4b69b6b1518520
20191864238 <56295781+MinazukiPER@users.noreply.github.com>
2019-11-02 16:46:38 +0800

    405
    homework

---
+++ 405/405.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int a,b;
+		scanf("%d",&a);
+if(a>1000||a<=0)
+{ printf("Please retry \n");}
+
+else
+
+{
+b=sqrt(a);
+
+}
+ printf("%d\n",b);
+ return 0;
+}
